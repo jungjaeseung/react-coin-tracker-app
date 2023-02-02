2e7a20d4af078381ff5e3569f858991bcd22052b
3Harang <jungjaeseung9475@gmail.com>
2023-02-02 19:54:06 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
 import styles from "./App.module.css";
 
 function App() {
-  return <div></div>;
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers?limit=50")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>The Coins! ({coins.length} coins!)</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <ul>
+          {coins.map((coin, index) => (
+            <li key={coin.id} value={index}>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price.toFixed(3)}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default App;
